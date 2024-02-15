a5b30091f3f0fb0e5e2ffc7be1500fa7163334ac
Homeo <40612643+homeopatix@users.noreply.github.com>
2024-02-15 20:26:45 +0100

    Create GlobalsDE.lua

---
+++ GlobalsDE.lua
@@ -0,0 +1,66 @@
+------------------------------------------------------------------------------------------
+-- GlobalsDE file
+-- Written by Homeopatix
+-- 22 Avril 2022
+------------------------------------------------------------------------------------------
+_G.T = {};
+------------------------------------------------------------------------------------------
+-- Command text --
+------------------------------------------------------------------------------------------
+T[ "PluginName" ] = "AnimalerieV2";
+T[ "Loaded" ] = "Geladen";
+T[ "Unactivated" ] = "Entladen";
+T[ "PluginText" ] = "Von Homeopatix";
+T[ "PluginEscEnable" ] = "Escape-Taste aktiviert";
+T[ "PluginEscDesable" ] = "Escape-Taste deaktiviert";
+T[ "PluginAltEnable" ] = "Alt-Taste aktiviert";
+T[ "PluginAltDesable" ] = "Alt-Taste deaktiviert";
+T[ "PluginClear" ] = "Schlie\195\159e die Tierhandlung ab";
+T[ "PluginClearAll" ] = "Alle Eintr\195\164ge l\195\182schen";
+T[ "PluginToggleOn" ] = "Fenster umschalten EIN";
+T[ "PluginToggleOff" ] = "Fenster umschalten AUS";
+T[ "PluginEscIconOn" ] = "Symbol anzeigen";
+T[ "PluginEscIconOff" ] = "Blendet das Symbol aus";
+T[ "PluginLocked" ] = "Symbole gesperrt";
+T[ "PluginUnlocked" ] = "Symbole entsperrt";
+T[ "Version" ] = Plugins[T[ "PluginName" ]]:GetVersion();
+T[ "PluginDateUpdate" ] = "21.12.2023";
+------------------------------------------------------------------------------------------
+-- plugin name and title --
+------------------------------------------------------------------------------------------
+T[ "PluginTextFam" ] = "Ihre Tierhandlung";
+T[ "PluginTextFood" ] = "Ihr Essen";
+T[ "PluginReleaseButton" ] = "Entfessle Vertrautes";
+T[ "PluginReleaseAlias" ] = "/begleiter freilassen";
+------------------------------------------------------------------------------------------
+-- default text --
+------------------------------------------------------------------------------------------
+T[ "PluginDefault" ] = "Standard-Zoohandlung";
+------------------------------------------------------------------------------------------
+-- Help text --
+------------------------------------------------------------------------------------------
+T[ "PluginHelp1" ] = " *** AnimalerieV2 Hilfe ***\n\n";
+T[ "PluginHelp2" ] = "/show Zeig die Tierhandlung\n";
+T[ "PluginHelp3" ] = "/hide Verstecke die Tierhandlung\n";
+T[ "PluginHelp4" ] = "/default F\195\188llt sich mit Basistieren\n";
+T[ "PluginHelp5" ] = "/lock Zum Sperren oder Entsperren der Symbole\n";
+T[ "PluginHelp6" ] = "/alt - Aktiviert oder deaktiviert die Alt-Taste zum Bewegen von Symbolen.\n";
+T[ "PluginHelp7" ] = "/toggle - Fenster ein-oder ausblenden.\n";
+T[ "PluginHelp8" ] = "/clear L\195\182schen Sie alle Hotkeys\n\n";
+T[ "PluginHelp9" ] = "Sie k\195\182nnen Haustiere per Drag-and-Drop in die erste Zeile und Futter in die zweite ziehen\nund mit dem Scrollrad einen Hotkey l\195\182schen Maus\n\n";
+------------------------------------------------------------------------------------------
+-- Options Window text --
+------------------------------------------------------------------------------------------
+T[ "PluginOptionsText" ] = "AnimalerieV2 Optionen";
+T[ "Option1" ] = "Rabe";
+T[ "Option2" ] = "B\195\164ren";
+T[ "Option3" ] = "Luchs";
+T[ "Option4" ] = "Adler";
+T[ "Option5" ] = "Tiger";
+T[ "Option6" ] = "W\195\164chter";
+T[ "Option7" ] = "Geist";
+T[ "PluginAleatoireGen" ] = "Setze zuf\195\164llige Haustiere";
+T[ "PluginOptionValidate" ] = "\195\164nderungen \195\188bernehmen";
+T[ "PluginAleatoire" ] = " Verwenden Sie zuf\195\164llige Haustiere";
+T[ "PluginSelected" ] = " Verwenden Sie ausgew\195\164hlte Haustiere";
+T[ "PluginAleatoireExplication" ] = " W\195\164hlen Sie die Haustiere aus, die Sie in die zuf\195\164llige \195\164nderung einbeziehen m\195\182chten. Basistiere sind erforderlich oder w\195\164hlen Sie nur ein Haustier pro Kollon aus und aktivieren Sie das Kontrollk\195\164stchen Ausgew\195\164hlte Haustiere verwenden";
