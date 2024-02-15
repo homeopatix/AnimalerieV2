5de299912a8e74c1a779a7912a8b5fec2736a0f1
Homeo <40612643+homeopatix@users.noreply.github.com>
2024-02-15 20:26:16 +0100

    Create GlobalsFR.lua

---
+++ GlobalsFR.lua
@@ -0,0 +1,66 @@
+------------------------------------------------------------------------------------------
+-- GlobalsFR file
+-- Written by Homeopatix
+-- 22 Avril 2022
+------------------------------------------------------------------------------------------
+_G.T = {};
+------------------------------------------------------------------------------------------
+-- Command text --
+------------------------------------------------------------------------------------------
+T[ "PluginName" ] = "AnimalerieV2";
+T[ "Loaded" ] = "charg\195\169";
+T[ "Unactivated" ] = "D\195\169sactiv\195\169";
+T[ "PluginText" ] = "Par Homeopatix";
+T[ "PluginEscEnable" ] = "Touche Escape activ\195\169";
+T[ "PluginEscDesable" ] = "Touche Escape desactiv\195\169";
+T[ "PluginAltEnable" ] = "Touche Alt activ\195\169";
+T[ "PluginAltDesable" ] = "Touche Alt desactiv\195\169";
+T[ "PluginClear" ] = "Efface l'Animalerie";
+T[ "PluginClearAll" ] = "Efface toutes les entr\195\169es";
+T[ "PluginToggleOn" ] = "Basculage de la fen\195\170tre ON";
+T[ "PluginToggleOff" ] = "Basculage de la fen\195\170tre OFF";
+T[ "PluginEscIconOn" ] = "Affiche l'icone";
+T[ "PluginEscIconOff" ] = "Masque l'icone";
+T[ "PluginLocked" ] = "Icones verrouill\195\169es";
+T[ "PluginUnlocked" ] = "Icones d\195\169verouill\195\169es";
+T[ "Version" ] = Plugins[T[ "PluginName" ]]:GetVersion();
+T[ "PluginDateUpdate" ] = "21.12.2023";
+------------------------------------------------------------------------------------------
+-- plugin name and title --
+------------------------------------------------------------------------------------------
+T[ "PluginTextFam" ] = "Votre Animalerie";
+T[ "PluginTextFood" ] = "Leur Nourriture";
+T[ "PluginReleaseButton" ] = "Lib\195\169rer Familier";
+T[ "PluginReleaseAlias" ] = "/fam libérer";
+------------------------------------------------------------------------------------------
+-- default text --
+------------------------------------------------------------------------------------------
+T[ "PluginDefault" ] = "Default l'Animalerie";
+------------------------------------------------------------------------------------------
+-- Help text --
+------------------------------------------------------------------------------------------
+T[ "PluginHelp1" ] = " *** AnimalerieV2 Aide ***\n\n";
+T[ "PluginHelp2" ] = "/show affiche l'animalerie\n";
+T[ "PluginHelp3" ] = "/hide cache l'animalerie\n";
+T[ "PluginHelp4" ] = "/default remplit avec les familiers de base\n";
+T[ "PluginHelp5" ] = "/lock pour verrouill\195\169 ou d\195\169verrouill\195\169 les icones\n";
+T[ "PluginHelp6" ] = "/alt - Active ou d\195\169sactive la touche alt pour le d\195\169placement de l'icon.\n";
+T[ "PluginHelp7" ] = "/toggle - affiche ou cache la fenetre.\n";
+T[ "PluginHelp8" ] = "/clear Efface tous les raccourcis\n\n";
+T[ "PluginHelp9" ] = "Vous pouvez faire glisser et d\195\169poser vos familier sur la premi\195\168re ligne\net de la nourriture sur la deuxi\195\168me et vous pouvez effacer un raccourci avec la molette de la souris\n\n";
+------------------------------------------------------------------------------------------
+-- Options Window text --
+------------------------------------------------------------------------------------------
+T[ "PluginOptionsText" ] = "AnimalerieV2 Options";
+T[ "Option1" ] = "Corbeau";
+T[ "Option2" ] = "Ours";
+T[ "Option3" ] = "Lynx";
+T[ "Option4" ] = "Aigle";
+T[ "Option5" ] = "Tigre";
+T[ "Option6" ] = "Guardien";
+T[ "Option7" ] = "Esprit";
+T[ "PluginAleatoireGen" ] = "D\195\169finis les familiers al\195\169atoire";
+T[ "PluginOptionValidate" ] = "Valider les changements";
+T[ "PluginAleatoire" ] = " Utiliser les familiers al\195\169atoire";
+T[ "PluginSelected" ] = " D\195\169finis les familiers s\195\169lectionn\195\169";
+T[ "PluginAleatoireExplication" ] = " S\195\169lectionner les familiers que vous voulez inclure dans le changement al\195\169atoire, le pet de base est obligatoire ou choisissez un seul familier par collonne et cocher la case D\195\169finis familiers s\195\169lectionner";
